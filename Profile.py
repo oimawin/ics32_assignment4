68c4e4c593d2994f3ac35ff27f50ac40898620bc
Emma Huynh <emmath@uci.edu>
2024-03-12 16:33:31 -0700

    Profile module docstring formatting

--- Profile.py
+++ Profile.py
@@ -11,20 +11,21 @@ import json, time
 from pathlib import Path
 
 
-"""
-DsuFileError is a custom exception handler that you should catch in your own code. It
-is raised when attempting to load or save Profile objects to file the system.
-
-"""
 class DsuFileError(Exception):
+    """
+    DsuFileError is a custom exception handler that you should catch in your own code. It
+    is raised when attempting to load or save Profile objects to file the system.
+
+    """
     pass
 
-"""
-DsuProfileError is a custom exception handler that you should catch in your own code. It
-is raised when attempting to deserialize a dsu file to a Profile object.
 
-"""
 class DsuProfileError(Exception):
+    """
+    DsuProfileError is a custom exception handler that you should catch in your own code. It
+    is raised when attempting to deserialize a dsu file to a Profile object.
+
+    """
     pass
 
 
@@ -43,7 +44,8 @@ class Post(dict):
         # Subclass dict to expose Post properties for serialization
         # Don't worry about this!
         dict.__init__(self, entry=self._entry, timestamp=self._timestamp)
-    
+
+
     def set_entry(self, entry):
         self._entry = entry 
         dict.__setitem__(self, 'entry', entry)
@@ -52,18 +54,20 @@ class Post(dict):
         if self._timestamp == 0:
             self._timestamp = time.time()
 
+
     def get_entry(self):
         return self._entry
-    
+
+
     def set_time(self, time:float):
         self._timestamp = time
         dict.__setitem__(self, 'timestamp', time)
-    
+
+
     def get_time(self):
         return self._timestamp
 
     """
-
     The property method is used to support get and set capability for entry and 
     time values. When the value for entry is changed, or set, the timestamp field is 
     updated to the current time.
@@ -71,8 +75,8 @@ class Post(dict):
     """ 
     entry = property(get_entry, set_entry)
     timestamp = property(get_time, set_time)
-    
-    
+
+
 class Profile:
     """
     The Profile class exposes the properties required to join an ICS 32 DSU server. You 
@@ -87,66 +91,68 @@ class Profile:
 
     """
 
+
     def __init__(self, dsuserver=None, username=None, password=None):
         self.dsuserver = dsuserver # REQUIRED
         self.username = username # REQUIRED
         self.password = password # REQUIRED
         self.bio = ''            # OPTIONAL
         self._posts = []         # OPTIONAL
-    
-    """
 
-    add_post accepts a Post object as parameter and appends it to the posts list. Posts 
-    are stored in a list object in the order they are added. So if multiple Posts objects 
-    are created, but added to the Profile in a different order, it is possible for the 
-    list to not be sorted by the Post.timestamp property. So take caution as to how you 
-    implement your add_post code.
-
-    """
 
     def add_post(self, post: Post) -> None:
+        """
+        add_post accepts a Post object as parameter and appends it to the posts list. Posts 
+        are stored in a list object in the order they are added. So if multiple Posts objects 
+        are created, but added to the Profile in a different order, it is possible for the 
+        list to not be sorted by the Post.timestamp property. So take caution as to how you 
+        implement your add_post code.
+
+        """
         self._posts.append(post)
 
-    """
-
-    del_post removes a Post at a given index and returns True if successful and False if 
-    an invalid index was supplied. 
-
-    To determine which post to delete you must implement your own search operation on 
-    the posts returned from the get_posts function to find the correct index.
-
-    """
 
     def del_post(self, index: int) -> bool:
+        """
+
+        del_post removes a Post at a given index and returns True if successful and False if 
+        an invalid index was supplied. 
+
+        To determine which post to delete you must implement your own search operation on 
+        the posts returned from the get_posts function to find the correct index.
+
+        """
         try:
             del self._posts[index]
             return True
         except IndexError:
             return False
-        
-    """
-    
-    get_posts returns the list object containing all posts that have been added to the 
-    Profile object
 
-    """
+
     def get_posts(self) -> list[Post]:
+        """
+    
+        get_posts returns the list object containing all posts that have been added to the 
+        Profile object
+
+        """
         return self._posts
 
-    """
 
-    save_profile accepts an existing dsu file to save the current instance of Profile 
-    to the file system.
-
-    Example usage:
-
-    profile = Profile()
-    profile.save_profile('/path/to/file.dsu')
-
-    Raises DsuFileError
-
-    """
     def save_profile(self, path: str) -> None:
+        """
+
+        save_profile accepts an existing dsu file to save the current instance of Profile 
+        to the file system.
+
+        Example usage:
+
+        profile = Profile()
+        profile.save_profile('/path/to/file.dsu')
+
+        Raises DsuFileError
+
+        """
         p = Path(path)
 
         if p.exists() and p.suffix == '.dsu':
@@ -159,20 +165,21 @@ class Profile:
         else:
             raise DsuFileError("Invalid DSU file path or type")
 
-    """
 
-    load_profile will populate the current instance of Profile with data stored in a 
-    DSU file.
-
-    Example usage: 
-
-    profile = Profile()
-    profile.load_profile('/path/to/file.dsu')
-
-    Raises DsuProfileError, DsuFileError
-
-    """
     def load_profile(self, path: str) -> None:
+        """
+
+        load_profile will populate the current instance of Profile with data stored in a 
+        DSU file.
+
+        Example usage: 
+
+        profile = Profile()
+        profile.load_profile('/path/to/file.dsu')
+
+        Raises DsuProfileError, DsuFileError
+
+        """
         p = Path(path)
 
         if p.exists() and p.suffix == '.dsu':

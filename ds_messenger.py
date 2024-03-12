d23d04ae2101ba7d87e838680a0f80cea34b33a5
Emma Huynh <emmath@uci.edu>
2024-03-12 16:08:50 -0700

    Wrote code to test if package_direct_msg function can correctly format a new message request

---
+++ ds_messenger.py
@@ -0,0 +1,11 @@
+
+# ds_messenger.py
+
+# Emma Huynh
+# emmath@uci.edu
+# 50385611
+
+import socket
+import json
+import time
+import ds_protocol
--- test_ds_messenge_protocol.py
+++ test_ds_messenge_protocol.py
@@ -10,6 +10,8 @@ import unittest
 import ds_protocol as dsp
 from ds_protocol import DataTuple
 
+def error_msg(expected, output):
+    return f"Expected output: {expected}. Actual output: {output}."
 
 class TestPckgDirectMsg(unittest.TestCase):
     def test_directmsg(self):
@@ -17,4 +19,10 @@ class TestPckgDirectMsg(unittest.TestCase):
         recipient = "ohhimark"
         expected = dsp.to_json({"token": "chicken_fries", "directmessage": {"entry": "I did not hit her! I did noooot.","recipient":"ohhimark", "timestamp": time.time()}})
         output = dsp.package_directmsg("chicken_fries", message=entry, recipient=recipient)
+        assert expected == output, error_msg(expected, output)
+
+class TestPckgNewMsgs(unittest.TestCase):
+    def test_new(self):
+        expected = '{"token": "chicken_fries", "directmessage": "new"}'
+        output = dsp.package_directmsg("chicken_fries", new=True)
         assert expected == output, error_msg(expected, output)

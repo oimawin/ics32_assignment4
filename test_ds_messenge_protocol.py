29c009f450be4346d2cd1fbee5fe0a7bd673f369
Emma Huynh <emmath@uci.edu>
2024-03-12 16:12:16 -0700

    Wrote code to test if extract_directmsgs can extract and output new and all messages from server

--- test_ds_messenge_protocol.py
+++ test_ds_messenge_protocol.py
@@ -10,6 +10,7 @@ import unittest
 import ds_protocol as dsp
 from ds_protocol import DataTuple
 
+    
 def error_msg(expected, output):
     return f"Expected output: {expected}. Actual output: {output}."
 
@@ -38,3 +39,27 @@ class TestExtractDirectsMsgs(unittest.TestCase):
         expected = DataTuple("ok", "Direct message sent", "")
         output = dsp.extract_directmsgs('{"response": {"type": "ok", "message": "Direct message sent"}}')
         assert expected == output, error_msg(expected, output)
+        
+    def test_extract_msgs(self):
+        example_response = {"response": 
+                {
+                    "type": "ok", 
+                    "messages": [{"message": "Hello User 1!", "from": "markb", "timestamp": "1603167689.3928561"}, 
+                                 {"message": "Bzzzzz", "from": "thebeemoviescript", "timestamp": "1603167689.3928561"}]
+                    }
+                }
+        example_response_str = '{"response": \
+            {"type": "ok", \
+            "messages": \
+                [{"message": "Hello User 1!", "from": "markb", "timestamp": "1603167689.3928561"}, \
+                {"message": "Bzzzzz", "from": "thebeemoviescript", "timestamp": "1603167689.3928561"}]}}'
+                
+        expected = DataTuple("ok", example_response['response']['messages'], "")
+        output = dsp.extract_directmsgs(example_response_str)
+        assert expected == output, error_msg(expected, output)
+        
+        
+        
+
+if __name__ == "__main__":
+    unittest.main()

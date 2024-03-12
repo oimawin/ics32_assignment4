f856e0552e03836fd03ec7e30551aaaaaa44284d
Emma Huynh <emmath@uci.edu>
2024-03-12 16:18:59 -0700

    test_join_svr function

--- test_ds_messenger.py
+++ test_ds_messenger.py
@@ -14,3 +14,11 @@ PORT = 3021
 
 user1 = DirectMessenger(SERVER, 'lunavoyager', 'velvetvortexpassword')
 user2 = DirectMessenger(SERVER, 'quantumquasar', 'lunarlegendpassword')
+
+class TestJoin(unittest.TestCase):
+    def test_join_svr(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as connection:
+            assert user1.join_server(connection, PORT)
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as connection: 
+            assert user2.join_server(connection, PORT)
